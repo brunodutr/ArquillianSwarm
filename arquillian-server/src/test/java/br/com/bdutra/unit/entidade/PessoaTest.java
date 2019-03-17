efa79d7dec2d380ff31c29c9c08827f0a5cde244
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-17 02:47:14 -0300

    Reverter último commit. Arquillian não funciona com JUNIT 5

--- arquillian-server/src/test/java/br/com/bdutra/unit/entidade/PessoaTest.java
+++ arquillian-server/src/test/java/br/com/bdutra/unit/entidade/PessoaTest.java
@@ -2,24 +2,19 @@ package br.com.bdutra.unit.entidade;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.Test;
 
 import br.com.bdutra.entidade.Pessoa;
 
 public class PessoaTest {
 
-	private Pessoa pessoa = new Pessoa();
-
-	@DisplayName("Deve atribuir um nome com sucesso")
-	@ParameterizedTest
-	@ValueSource(strings = { "Teste", "Teste 2" })
-	public void test(String nome) {
-
-		pessoa.setNome(nome);
-
-		assertEquals(pessoa.getNome(), nome);
+	@Test
+	public void test() {
+		Pessoa pessoa = new Pessoa();
+		
+		pessoa.setNome("Teste");
+		
+		assertEquals(pessoa.getNome(), "Teste");
 	}
 
 }
