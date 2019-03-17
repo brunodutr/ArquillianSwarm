1916278616255cf1fbe9ce7ed2ac2bb327a49dd6
Bruno Dutra <brun_dutra@hotmail.com>
2019-03-17 02:25:33 -0300

    Configurar JUNIT 5 no Projeto

--- arquillian-server/src/test/java/br/com/bdutra/unit/entidade/PessoaTest.java
+++ arquillian-server/src/test/java/br/com/bdutra/unit/entidade/PessoaTest.java
@@ -2,19 +2,24 @@ package br.com.bdutra.unit.entidade;
 
 import static org.junit.Assert.assertEquals;
 
-import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import br.com.bdutra.entidade.Pessoa;
 
 public class PessoaTest {
 
-	@Test
-	public void test() {
-		Pessoa pessoa = new Pessoa();
-		
-		pessoa.setNome("Teste");
-		
-		assertEquals(pessoa.getNome(), "Teste");
+	private Pessoa pessoa = new Pessoa();
+
+	@DisplayName("Deve atribuir um nome com sucesso")
+	@ParameterizedTest
+	@ValueSource(strings = { "Teste", "Teste 2" })
+	public void test(String nome) {
+
+		pessoa.setNome(nome);
+
+		assertEquals(pessoa.getNome(), nome);
 	}
 
 }
